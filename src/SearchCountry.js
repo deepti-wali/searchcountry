3f8a0ee3211a90063fce6d2bdc437a214256e206
Deepti <134826480+deepti-wali@users.noreply.github.com>
2023-12-24 14:44:52 +0530

    first commit

--- src/SearchCountry.js
+++ src/SearchCountry.js
@@ -2,67 +2,87 @@ import React, { useState, useEffect } from "react";
 
 function CountryDisplay() {
   const [countries, setCountries] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [error, setError] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    fetchCountries();
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => setCountries(data))
+      .catch((error) => console.error("Error fetching data: ", error));
   }, []);
 
-  const fetchCountries = async () => {
-    try {
-      const response = await fetch("https://restcountries.com/v3.1/all");
-      if (!response.ok) {
-        throw new Error("Failed to fetch data");
-      }
-      const data = await response.json();
-      setCountries(data);
-    } catch (err) {
-      setError(err);
-      console.error("Error fetching data:", err);
-    }
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  const cardStyle = {
+    width: "200px",
+    border: "1px solid #ccc",
+    borderRadius: "10px",
+    margin: "10px",
+    padding: "10px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
   };
 
-  const filteredCountries = countries.filter((country) =>
-    country.name.common.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  const containerStyle = {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "80px 20px",
+  };
+
+  const headerStyle = {
+    position: "fixed",
+    top: "0",
+    left: "0",
+    width: "100%",
+    backgroundColor: "#f8f8f8",
+    padding: "10px",
+    boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+    zIndex: "1000",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  const searchStyle = {
+    width: "50%",
+    padding: "10px",
+    fontSize: "16px",
+  };
+
+  const imageStyle = {
+    width: "100px",
+    height: "100px",
+  };
 
   return (
-    <div>
-      <input
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-        type="text"
-        placeholder="Search countries..."
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-      />
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
+    <>
+      <div style={headerStyle}>
+        <input
+          type="text"
+          placeholder="Search for countries..."
+          style={searchStyle}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+      <div style={containerStyle}>
         {filteredCountries.map((country) => (
-          <div
-            key={country.name.common}
-            style={{
-              width: "200px",
-              margin: "10px",
-              padding: "10px",
-              border: "1px solid #ccc",
-              borderRadius: "5px",
-              textAlign: "center",
-            }}
-          >
+          <div key={country.cca3} style={cardStyle}>
             <img
               src={country.flags.png}
-              alt={country.name.common + " flag"}
-              style={{ width: "100px", height: "auto" }}
+              alt={`Flag of ${country.name.common}`}
+              style={imageStyle}
             />
-            <p>{country.name.common}</p>
+            <h2>{country.name.common}</h2>
           </div>
         ))}
       </div>
-    </div>
+    </>
   );
 }
 
